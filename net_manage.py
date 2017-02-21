afdd7dbcc5ad3cdcde48c542075096d014446717
MankwanHo <gh_william@hotmail.com>
2017-02-21 17:48:57 +0000

    Update net_manage.py

--- net_manage.py
+++ net_manage.py
@@ -10,7 +10,7 @@ MQTT_OUT_TOPIC='esys/plus_one_second/device_out'
 MQTT_USER_IN_TOPIC='esys/plus_one_second/user_in'
 
 #this function is to set up basic wifi connection with the modem
-#
+#This function only uses the wifi set up for this course
 def wifi_connect(sta_if,WIFI_NAME,WIFI_PASSWORD):
     sta_if.connect(WIFI_NAME, WIFI_PASSWORD)
     time.sleep_ms(500)
@@ -19,16 +19,17 @@ def wifi_connect(sta_if,WIFI_NAME,WIFI_PASSWORD):
         sta_if.connect(WIFI_NAME, WIFI_PASSWORD)
         time.sleep_ms(500)
         wificonnected=sta_if.isconnected()
-       
+
+#This is to disconnect the Wifi internally       
 def internal_wifi_disconnect(ap_if):
     ap_if.active(False)
 
-
+#this is to connect to the MQTT server in EERover
 def mqtt_connect(client):
     global MQTT_OUT_TOPIC
     client.connect()
     send_data(client,{'message':'connected'})
-
+#Function for sending message in json format
 def send_data(client,message):
     global MQTT_OUT_TOPIC
     payload = json.dumps(message)
@@ -43,19 +44,19 @@ def send_data(client,message):
 #    payload = json.dumps(message)
 #    client.publish(MQTT_USER_IN_TOPIC,bytes(payload,'utf-8'))
 
-    
+#This is a call back function specific for retrieving real time clock    
 def sub_time(topic,msg):
     global timenow
     timenow = json.loads(msg)['date']
-
+    
+#call back function for receiving user message
 def sub_user_msg(topic,msg):
     global user_msg
     tmpp = json.loads(msg)
     if 'message' in tmpp:
         user_msg = tmpp['message']
-    
-
-
+        
+#Function for setting up rtc
 def setting_rtc(rtc):
     Yr=int(timenow[0:4])
     Mnh=int(timenow[5:7])
@@ -66,7 +67,8 @@ def setting_rtc(rtc):
     rtc.datetime((Yr, Mnh, Dy, 1, Hr, Min, Snd, 0))
     print(rtc.datetime())
 
-
+#This is the higheset level function for rtc set up including calling MQTT
+#and calling set up function
 def setting_datetime(rtc,client):
     global TIME_TOPIC
     client.set_callback(sub_time)
@@ -75,12 +77,14 @@ def setting_datetime(rtc,client):
     setting_rtc(rtc)
     client.disconnect()
 
-
+#This is to set up client and subscribe to MQTT function
 def subscribe_message(client):
     global MQTT_USER_IN_TOPIC
     client.set_callback(sub_user_msg)
     client.subscribe(MQTT_USER_IN_TOPIC)
-
+    
+#This is function to retrieve user function
+#'default' null for user_msg allow upper level function to reliease no message is received
 def check_message(client):
     global user_msg
     user_msg='None'
